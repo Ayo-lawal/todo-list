79ea3e578517badea96d478178061a1016191845
Majeedat <62648680+Ayo-lawal@users.noreply.github.com>
2024-01-19 10:27:42 +0100

    TodoList - React Tryout

---
+++ src/components/TodoList.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import TodoItem from './TodoItem';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, { id: Date.now(), text: newTodo }]);
+      setNewTodo('');
+    }
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} onDelete={deleteTodo} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+
+export default TodoList;
